af42495b64601dd190e8905c1bcbbbc871f5d7c9
RN <hsiangjenli@gmail.com>
2023-03-01 14:39:56 +0800

    feat: update abs path

--- auto_update.py
+++ auto_update.py
@@ -17,9 +17,11 @@ def SetEnvironemnt(folder: str, template: str):
 config = LoadConfig(fname='config/_readme.toml')
 
 template_md = SetEnvironemnt(folder='static/templates', template='README.md')
-template_html = SetEnvironemnt(folder='static/templates', template='index.html')
+template_html = SetEnvironemnt(folder=['static/templates', 'static'], template='index.html')
 
 meta_data = {
+    # 'static': 'file:///C:/Users/ASUS/Documents/Github/hsiangjenli/static/',
+    'static': 'https://hsiangjenli.github.io/hsiangjenli/static/',
     'author':config['Author'],
     'educations':config['Education'],
     'awards':config['Award'],
